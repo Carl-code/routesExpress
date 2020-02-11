047b7e8b59fff3f9f8781572ac7d77161a56060e
Carl-code <arimatea865@gmail.com>
2020-02-11 15:53:09 -0600

    ADD: a POST Method to add a new koder

--- index.js
+++ index.js
@@ -2,12 +2,45 @@
 const express = require('express')
 const app = express()
 
+const koders = [
+  {
+    name: 'Carlos',
+    age: 25,
+    generation: 4
+  },
+  {
+    name: 'Pedro',
+    age: 28,
+    generation: 4
+  },
+  {
+    name: 'Jose',
+    age: 23,
+    generation: 5
+  }
+]
 //Creación de rutas
 app.get('/koders', (request, response) => {
     response.json({
         message: 'todos los koders',
         data: {
-            koders: []
+            koders: koders
+        }
+    })
+})
+
+app.post('/koders', (request, response) => {
+    const newKoder = {
+        name: 'Monse',
+        age: 23,
+        generation: 4
+    }
+    koders.push(newKoder)
+
+    response.json({
+        message: 'New Koder added',
+        data: {
+            koders: koders
         }
     })
 })
